31f2b273bbeea4ef3ad2f844944f1e10094fb737
ghost <zhao_chenhui_scu@163.com>
2016-02-04 10:31:49 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # py-ngx
+
 用python 模拟nginx 制作一套master-slave 服务程序
+
+## 使用
+
+### 启动
+
+    ./pyngx.sh
+    
+### 重启
+
+    ./pyngx.sh reload
+    
+### 停止
+
+    ./pyngx.sh close
--- pyngx.sh
+++ pyngx.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-pid=`cat py-ngx.pid`
 
 if [ "$1" == "reload" ]; then
+    pid=`cat py-ngx.pid`
     echo pid = ${pid} reloading...
     kill -10 ${pid}
 elif [ "$1" == "close" ]; then
+    pid=`cat py-ngx.pid`
     echo pid = ${pid} close...
     kill -15 ${pid}
 else
